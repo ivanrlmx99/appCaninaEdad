b34d236cbe2bcbe95e6696dfc057da44cfa2ab37
Ivan rl <ivanlaureano77@gmail.com>
2020-04-18 21:15:07 -0500

    se termino la app edad canina

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView answerText=(TextView)findViewById(R.id.text_answer);
+        final EditText ageDog=(EditText) findViewById(R.id.edit_text);
+
+        Button myBoton=(Button)findViewById(R.id.my_botton);
+
+
+        myBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ageString=ageDog.getText().toString();
+                int edadInt=Integer.parseInt(ageString);
+                answerText.setText("SI FUERAS PERRO TU EDAD SERIA:"+(edadInt*6));
+            }
+        });
+
+
     }
 }
